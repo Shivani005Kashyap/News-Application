7306e254e2cbe569b222d6d1d9e3997c50f95608
Shivani Kashyap <shivani26kashyap2005@gmail.com>
2024-12-22 11:06:50 +0530

    Add files via upload

---
+++ NewsItems.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function NotesItems() {
+  return (
+    <div className='my-3'>
+    <div className="card" style={{ width: "18rem" }}>
+      <img src="" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Description</p>
+        <p className="card-text"> Published By Author on Date</p>
+        <a rel="noreferrer" href="/" target="_blank" className="btn btn-sm btn-primary">Read More</a>
+      </div>
+    </div>
+  </div>
+
+  )
+}
